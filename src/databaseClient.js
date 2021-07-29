f1be68c9ca38bbb6631fd2901abe3f70a23974a6
derekzx <chinzhixian@gmail.com>
2021-07-29 04:13:06 -0700

    remove secrets

--- src/databaseClient.js
+++ src/databaseClient.js
@@ -3,29 +3,11 @@ const { createPool } = require('mysql');
 
 // Database Connection for Production
 
-// const pool = mysql.createPool({
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_DATABASE,
-//   socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
-// });
-
-// const pool = mysql.createPool({
-//   user: "root",
-//   password: "gpEp63bvAOd5oA4y",
-//   database: "utp_dex",
-//   socketPath: "/cloudsql/utopia-315014:us-west1:utp-dex-db",
-// });
-
-// module.exports = pool;
-
-// Database Connection for Development
-
 const pool = mysql.createPool({
-  user: "root",
-  password: "gpEp63bvAOd5oA4y",
-  database: "utp_dex",
-  socketPath: "/cloudsql/utopia-315014:us-west1:utp-dex-db",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
 });
 
 module.exports = pool;
